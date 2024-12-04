0a71177d51ed7e799e8dd080d53e8f4421a0b268
Omar Mahmoud Mohammed <omarmah1907@gmail.com>
2024-12-04 18:25:00 +0200

    feature:implement room service

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 import { UpdateRoomDto } from './dto/update-room.dto';
+import { AvailabilityStatusEnum } from './enum/available-status.enum';
 
 @Injectable()
 export class RoomsService {
@@ -17,6 +18,7 @@ export class RoomsService {
     try {
       const room = this.roomRepository.create(createRoomDto);
       await this.roomRepository.save(room);
+      this.logger.info('Room created successfully');
       return {
         statusCode: HttpStatus.CREATED,
         message: 'Room created successfully',
@@ -31,12 +33,79 @@ export class RoomsService {
     }
   }
 
-  async update(updateRoomDto: UpdateRoomDto, id: number) {
+  async update(id: number, updateRoomDto: UpdateRoomDto) {
     try {
       const room = await this.roomRepository.findOne({ where: { id } });
       if (!room) {
-        throw new HttpException('Room not found', HttpStatus.NOT_FOUND);
+        this.logger.error(`No Room with id: ${id}`);
+        return new HttpException('Room not found', HttpStatus.NOT_FOUND);
       }
-    } catch (error) {}
+      Object.assign(room, updateRoomDto);
+      await this.roomRepository.save(room);
+      this.logger.info('Room Updated successfully');
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Room updated successfully',
+        room,
+      };
+    } catch (error) {
+      this.logger.error({ error }, 'Failed to create room');
+      throw new HttpException(
+        'Failed to update room',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findAll(query: {
+    capacity?: number;
+    availabilityStatus?: AvailabilityStatusEnum;
+  }) {
+    try {
+      const { capacity, availabilityStatus } = query;
+      const whereClause: any = {};
+      if (capacity) {
+        whereClause.capacity = capacity;
+      }
+      if (availabilityStatus) {
+        whereClause.availabilityStatus = availabilityStatus;
+      }
+
+      const rooms = await this.roomRepository.find({ where: whereClause });
+      this.logger.info('Rooms found');
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Rooms found',
+        rooms,
+      };
+    } catch (error) {
+      this.logger.error({ error }, 'Failed to retrieve rooms');
+      throw new HttpException(
+        'Failed to create room',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findOne(id: number) {
+    try {
+      const room = await this.roomRepository.findOne({ where: { id } });
+      if (!room) {
+        this.logger.error(`No Room with id: ${id}`);
+        return new HttpException('Room not found', HttpStatus.NOT_FOUND);
+      }
+      this.logger.info('Room found');
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Room found',
+        room,
+      };
+    } catch (error) {
+      this.logger.error({ error }, 'Failed to retrieve rooms');
+      throw new HttpException(
+        'Failed to retrieve room',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
